a7c544a168f2e8c6a1cbbd5d7c2cb140e80cfb89
guvdsz <gustavovianasouzacontatos@gmail.com>
2026-04-23 18:28:00 -0300

    fix; adicionado um endpoint heath na aplicação, visando evitar a hibernação da API (Cold start)

---
+++ src/backend/Feature/System/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoraLaBackend.Feature.System
+{
+    [Route("health")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Check()
+        {
+            return Ok(new
+            {
+                status = "online",
+                timestamp = DateTime.UtcNow
+            });
+        }
+    }
+}
