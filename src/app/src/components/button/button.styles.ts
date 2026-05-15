d5957b22a72a53e6e33850d468f1962a54769ebf
Willy Teixeira <1567225@sga.pucminas.br>
2026-05-14 21:40:59 -0300

    fix: corrige customização do botão

--- src/app/src/components/button/button.styles.ts
+++ src/app/src/components/button/button.styles.ts
@@ -2,57 +2,69 @@ import { StyleSheet } from "react-native";
 
 export const PrimaryStyles = StyleSheet.create({
   labelStyle: {
+    width: '100%',
+    textAlign: 'center',
     fontSize: 14,
     fontWeight: 500,
-    color: "#FFFFFF"
+    color: "#FFFFFF",
   },
   containerStyle: {
+    alignItems: "center",
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 12,
     backgroundColor: "#EC5B13",
-  }
-})
+  },
+});
 
 export const SecondaryStyles = StyleSheet.create({
   labelStyle: {
+    width: '100%',
+    textAlign: 'center',
     fontSize: 14,
     fontWeight: 500,
-    color: "#EC5B13"
+    color: "#EC5B13",
   },
   containerStyle: {
+    alignItems: "center",
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 12,
     backgroundColor: "#ec5b136c",
-  }
-})
+  },
+});
 
 export const TertiaryStyles = StyleSheet.create({
   labelStyle: {
+    width: '100%',
+    textAlign: 'center',
     fontSize: 14,
     fontWeight: 700,
     color: "#EC5B13",
-    textTransform: 'uppercase',
+    textTransform: "uppercase",
   },
   containerStyle: {
+    alignItems: "center",
     paddingVertical: 10,
     paddingHorizontal: 20,
-  }
-})
+  },
+});
 
 export const PrimaryStylesOutline = StyleSheet.create({
   labelStyle: {
+    width: '100%',
+    textAlign: 'center',
     fontSize: 14,
     fontWeight: 500,
     color: "#EC5B13",
   },
   containerStyle: {
+    alignItems: "center",
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 12,
     borderWidth: 1,
     borderColor: "#EC5B13",
     backgroundColor: "#FFFFFF",
-  }
-})
+  },
+});
--- src/app/src/components/button/button.tsx
+++ src/app/src/components/button/button.tsx
@@ -1,18 +1,27 @@
-import { FC } from 'react';
-import { Text, TouchableOpacity, } from 'react-native';
-import { TButtonViewProps } from './types';
+import { FC } from "react";
+import { Text, TouchableOpacity } from "react-native";
+import { TButtonViewProps } from "./types";
 
 /**
  * O arquivo que leva o nome da pasta é a camada de visualização. Esse arquivo
  * quando possível, não deve conter lógica para que se mantenha a separação.
- * Lembre-se de que isso é uma recomendação e podem haver casos onde não 
- * há outro caminho. Se necessário por, adicione apenas o estritamente necessário. 
+ * Lembre-se de que isso é uma recomendação e podem haver casos onde não
+ * há outro caminho. Se necessário por, adicione apenas o estritamente necessário.
  */
-export const ButtonView: FC<TButtonViewProps> = (props) => {
+export const ButtonView: FC<TButtonViewProps> = ({
+  children,
+  typeStyle,
+  containerProps,
+  labelProps,
+}) => {
   return (
-    <TouchableOpacity onPress={props.onPress} style={props.style.containerStyle}>
-      <Text style={props.style.labelStyle}>{props.children}</Text>
+    <TouchableOpacity
+      {...containerProps}
+      style={[typeStyle.containerStyle, containerProps?.style]}
+    >
+      <Text {...labelProps} style={[typeStyle.labelStyle, labelProps?.style]}>
+        {children}
+      </Text>
     </TouchableOpacity>
-  )
-}
-
+  );
+};
